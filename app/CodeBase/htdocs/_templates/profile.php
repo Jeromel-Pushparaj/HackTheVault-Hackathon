5dc1e92073698112e847e915e94bfd6b927464c9
Jeromel Pushparaj <jeromal03@gmail.com>
2025-04-29 12:11:00 +0530

    Deleting

--- app/CodeBase/htdocs/_templates/profile.php
+++
@@ -1,4 +0,0 @@
-<?php
-Session::loadTemplate('profile/apikey');
-Session::loadTemplate('profile/sidebar');
-?>
--- create-hackathon.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/Jeromel-Pushparaj/BoilerPlate_Docker.git
-
-cd BoilerPlate_Docker
-
-docker compose up -d
